dcb2393a6c81d3544c6b80b0f7fbcde40946533e
PiPihodez <mixa_and_java@mail.ru>
2022-11-03 15:46:21 +0300

    add task 4

--- src/lesson_03/homework_02/Task4.java
+++ src/lesson_03/homework_02/Task4.java
@@ -1,4 +1,24 @@
 package lesson_03.homework_02;
 
+import java.util.Scanner;
+
+/*Написать программу, которая будет проверять число, которое мы подадим ей.
+Если число положительное, то увеличить его в два раза.
+Если число отрицательное, то прибавить единицу.
+Если введенное число равно нулю, необходимо вывести ноль.
+Вывести результат в консоль.*/
 public class Task4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int x = sc.nextInt();
+
+        if (x > 0) {
+            System.out.println(x * 2);
+        } else if (x < 0) {
+            System.out.println(x + 1);
+        } else if (x == 0) {
+            System.out.println(x);
+        }
+    }
 }
