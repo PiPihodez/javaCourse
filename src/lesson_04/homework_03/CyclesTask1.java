e48f50632b7d930830785a835837e2ddfc880af9
PiPihodez <mixa_and_java@mail.ru>
2022-11-06 14:52:45 +0300

    add CyclesTask1

--- src/lesson_04/homework_03/CyclesTask1.java
+++ src/lesson_04/homework_03/CyclesTask1.java
@@ -14,5 +14,12 @@ public class CyclesTask1 {
         int m = sc.nextInt();
         int n = sc.nextInt();
 
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("8");
+            }
+            System.out.println("");
+        }
+
     }
 }
