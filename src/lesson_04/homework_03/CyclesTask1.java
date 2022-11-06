867807e653538b5f08139866a5c230da84cf38ac
PiPihodez <mixa_and_java@mail.ru>
2022-11-06 12:02:07 +0300

    add homework_03

---
+++ src/lesson_04/homework_03/CyclesTask1.java
@@ -0,0 +1,18 @@
+package lesson_04.homework_03;
+
+import java.util.Scanner;
+
+/*Ввести с клавиатуры два числа m и n.
+Используя цикл for вывести на экран прямоугольник размером m на n из восьмёрок.
+
+Пример: m=2, n=4
+8888
+8888*/
+public class CyclesTask1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+    }
+}
