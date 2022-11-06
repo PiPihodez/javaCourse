2f9712190bab435c1fab571f4b0dde2cda1a0b60
PiPihodez <mixa_and_java@mail.ru>
2022-11-06 18:55:28 +0300

    add CyclesTask6

--- src/lesson_04/homework_03/CyclesTask6.java
+++ src/lesson_04/homework_03/CyclesTask6.java
@@ -1,4 +1,7 @@
 package lesson_04.homework_03;
+
+import java.util.Scanner;
+
 /*Ввести с клавиатуры строку name.
 Ввести с клавиатуры дату рождения (три числа): y, m, d.
 
@@ -10,4 +13,23 @@ package lesson_04.homework_03;
 Меня зовут Вася.
 Я родился 15.2.1988*/
 public class CyclesTask6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int y;
+        int m;
+        int d;
+
+        System.out.print("Введите имя: ");
+        String name = sc.nextLine();
+
+        System.out.print("Введите год: ");
+        y = sc.nextInt();
+        System.out.print("Введите месяц: ");
+        m = sc.nextInt();
+        System.out.print("Введите день: ");
+        d = sc.nextInt();
+
+        System.out.println("Меня зовут " + name + "." + "\nЯ родился " + d + "." + m + "." + y);
+
+    }
 }
