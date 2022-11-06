95c94ca8e05e2ac52f4a3c9c7bc9ea72f21e9dc6
PiPihodez <mixa_and_java@mail.ru>
2022-11-06 18:33:05 +0300

    add CyclesTask4

--- src/lesson_04/homework_03/CyclesTask4.java
+++ src/lesson_04/homework_03/CyclesTask4.java
@@ -1,4 +1,7 @@
 package lesson_04.homework_03;
+
+import java.util.Scanner;
+
 /*Вводить с клавиатуры числа.
 Если пользователь ввел -1, вывести на экран сумму всех введенных чисел и завершить программу.
 -1 должно учитываться в сумме.
@@ -13,4 +16,23 @@ while (true) {
 
 */
 public class CyclesTask4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a;
+        int b = 0;
+
+        while (true) {
+            a = sc.nextInt();
+            if (a != -1) {
+                b = b + a;
+            } else if (a == -1) {
+                System.out.println("Вы ввели: " + a);
+                b = b + a;
+                break;
+            }
+        }
+        System.out.println("Сумма: " + b);
+    }
 }
+
