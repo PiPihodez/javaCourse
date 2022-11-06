8ac91b86c75a7f8294b1a830ec2829077e3d9f56
PiPihodez <mixa_and_java@mail.ru>
2022-11-06 14:01:38 +0300

    add CyclesTask2

--- src/lesson_04/homework_03/CyclesTask2.java
+++ src/lesson_04/homework_03/CyclesTask2.java
@@ -16,6 +16,12 @@ package lesson_04.homework_03;
 public class CyclesTask2 {
     public static void main(String[] args) {
 
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("8");
+            }
+            System.out.println("");
+        }
 
 
     }
